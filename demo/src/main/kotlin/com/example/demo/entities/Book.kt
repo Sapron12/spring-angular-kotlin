becf9b4d96bd2513a8f7f224b706c655bfa7341a
Egor Medvedev <e.medvedev@itransition.com>
2020-08-20 12:07:13 +0300

    front

--- demo/src/main/kotlin/com/example/demo/entities/Book.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demo.entities
-
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-
-@Entity
-class Book(@Id @GeneratedValue var id: Long, val title: String)
